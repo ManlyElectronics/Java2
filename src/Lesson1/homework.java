f4dcc81437654cdcb7a2019d326f98f1b7990739
DimitriRytsk <sales@manlyelectronics.com.au>
2020-12-25 19:09:58 +1100

    Initial commit

---
+++ src/Lesson1/homework.java
@@ -0,0 +1,6 @@
+package Lesson1;
+
+public class homework {
+    public static void main(String[] args) {
+
+}
---
+++ src/Lesson1/interfaces.java
@@ -0,0 +1,8 @@
+package Lesson1;
+
+public class interfaces {
+    public static void main(String[] args) {
+        System.out.println("heko");
+
+    }
+}
