d6d0eb66ec0ed16de42c0789658a9c9ccd9be8b5
DimitriRytsk <sales@manlyelectronics.com.au>
2020-12-28 22:59:40 +1100

    cleaned

--- src/Lesson1/HomeWork/Task1/ver2/Participant.java
+++ src/Lesson1/HomeWork/Task1/ver2/Participant.java
@@ -2,10 +2,5 @@ package Lesson1.HomeWork.Task1.ver2;
 
 interface Participant {
     default void Pass(Obstacle obstacle) {
-        System.out.println(obstacle.getOLength());
-        System.out.println(obstacle.getOHeight());
-        System.out.println(obstacle.getOName());
-        System.out.println("Участник прошёл препядсвие");
     }
-
 }
